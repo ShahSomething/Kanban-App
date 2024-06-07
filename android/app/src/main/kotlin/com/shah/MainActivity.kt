b27b56ddd873f5a010729fd9c8ff45d0221ff100
shahsomething <shahsomething@yahoo.com>
2024-06-07 14:50:36 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/shah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shah.kanban
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:kanban/app/app.dart';
+import 'package:kanban/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
